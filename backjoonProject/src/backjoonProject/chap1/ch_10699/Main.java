78f188078027b20f134197bd99ba18990b8d8cf8
詩音 <108321928+coollee1001@users.noreply.github.com>
2023-03-27 15:37:56 +0900

    add

---
+++ backjoonProject/src/backjoonProject/chap1/ch_10699/Main.java
@@ -0,0 +1,15 @@
+package backjoonProject.chap1.ch_10699;
+
+import java.time.LocalDate;
+
+/**
+ * https://www.acmicpc.net/problem/10699
+ * 
+ * @author siwon
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(LocalDate.now());
+	}
+}
---
+++ backjoonProject/src/backjoonProject/chap1/ch_10718/Main.java
@@ -0,0 +1,15 @@
+package backjoonProject.chap1.ch_10718;
+
+/**
+ * https://www.acmicpc.net/problem/10718
+ * 
+ * @author siwon
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		String str = "강한친구 대한육군";
+		System.out.println(str);
+		System.out.println(str);
+	}
+}
